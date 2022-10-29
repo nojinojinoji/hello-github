85d08d0a37cd502be8feaf72e09370f3c16c1746
nojiriakari <akari.nojiri.3t@stu.hosei.ac.jp>
2022-10-29 23:58:06 +0900

    更新済み

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # html.escape()　＝ 意味のあるものを無向にする
 # render_template＝ HTMLを表示する（Flaskの機能）
 
-
+import email
 from cv2 import AKAZE_DESCRIPTOR_KAZE
-from flask import Flask, request, session, redirect, render_template
+from flask import Flask, request, redirect,  session, render_template
+from scipy.fft import idctn
 from werkzeug.security import generate_password_hash as gph
 from werkzeug.security import check_password_hash as cph
 from datetime import timedelta
@@ -25,64 +26,161 @@ def connect():
                 charset="utf8")
         return con
 
+
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
+app.permanent_session_lifetime = timedelta(minutes=60)
 
 @app.route("/")
 def root_page():
-        return redirect("adduser")
+        return redirect("make")
 
 #アカウント登録
-@app.route("/adduser", methods=["GET","POST"])
-def adduser_page():
-        # GET＝ページ表示
-        if request.method == "GET": 
-                return render_template('adduser.html')
-        # POST＝DB登録
+@app.route("/make", methods=["GET","POST"])
+def make(): 
+        if request.method == "GET":
+                return render_template('make.html')
         elif request.method == "POST":
-                id = request.form["id"] #idを受け取る
-                password = request.form["password"] #passwordを受け取る
-                name = request.form["name"] #nameを受け取る
-                birth = request.form["birth"] #birthを受け取る
-                sex = request.form["sex"] #sexを受け取る
-                money = request.form["money"]
+                email = request.form["email"] 
+                passwd = request.form["passwd"]
+                id =  request.form["id"]
+                name = request.form["name"] 
+                birth = request.form["birth"] 
+                hashpass=gph(passwd)
 
                 con = connect()
                 cur = con.cursor()
-                #INSERTでタイトル＆説明＆投稿時間をDBに格納
+                # メールアドレスの被りがあるか検索
                 cur.execute("""
-                INSERT INTO user
-                (id,password,name,birth,sex)
-                VALUES(%(id)s,%(password)s,%(name)s,%(birth)s,%(sex)s)""",
-                {"id":id, "password":password, "name":name, "birth":birth, "sex":sex })
-                cur.execute("""
-                INSERT INTO balance
-                (id,date,puramai,money,m_id)
-                VALUES(%(id)s,%(date)s,%(puramai)s,%(money)s,%(m_id)s)""",
-                {"id":id, "date":str(datetime.datetime.today()), "puramai":0, "money":money, "m_id":id })
+                SELECT * FROM users WHERE email=%(email)s""",
+                {"email":email})
+                data=[]
+                for row in cur:
+                    data.append(row)
+                # メールアドレスが1つ以上登録されていた場合⇒登録不可
+                if len(data)!=0:
+                    return render_template("make.html",msg="既に存在するメールアドレスです")
                 con.commit()
                 con.close()
-                #最後にタイトルを用いてホーム画面にリダイレクトする
                 
-                return redirect("finish")
+                # idの被りがあるか検索
+                con = connect()
+                cur = con.cursor()
+                cur.execute("""
+                SELECT * FROM users WHERE id=%(id)s""",
+                {"id":id})
+                data=[]
+                for row in cur:
+                    data.append(row)
+                # idが1つ以上登録されていた場合⇒登録不可
+                if len(data)!=0:
+                    return render_template("make.html",msg="既に存在するIDです")
+                con.commit()
+                con.close()
 
-# 登録完了
-@app.route('/finish' ,methods=["POST","GET"]) 
-def finish(): 
-        # GET＝ページ表示
+                con = connect()
+                cur = con.cursor()
+                # メアド&IDの被りがない場合に登録ができる
+                cur.execute("""
+                INSERT INTO users
+                (email,passwd,id,name,birth)
+                VALUES(%(email)s,%(hashpass)s,%(id)s,%(name)s,%(birth)s)""",
+                {"email":email, "hashpass":hashpass, "id":id, "name":name, "birth":birth})
+                con.commit()
+                con.close()
+
+                con = connect()
+                cur = con.cursor()
+                # 預金テーブルにも登録する
+                cur.execute("""
+                INSERT INTO balance
+                (id,updatetime,money)
+                VALUES(%(id)s,%(updatetime)s,%(money)s)""",
+                {"id":id, "updatetime":str(datetime.datetime.today()), "money":0})
+                con.commit()
+                con.close()
+                
+                return render_template("info.html",email=email,passwd=passwd,name=name,birth=birth,id=id)
+
+
+# ログイン
+@app.route("/login", methods=["GET","POST"])
+def login():
         if request.method == "GET": 
-                return render_template('finish.html')
+            # ログイン画面ではセッションを破棄
+            session.clear() 
+            return render_template('login.html') 
         elif request.method == "POST":
-	        return redirect("mypage")
+            id = request.form["id"] 
+            passwd = request.form["passwd"]
+            con = connect()
+            cur = con.cursor()
+            # データベースに入力されたIDがあるか探す
+            cur.execute("""
+                SELECT name,birth,email,id,passwd
+                FROM users
+                WHERE id=%(id)s""",{"id":id})
+            data=[]
+
+            for row in cur:
+                data.append([row[0],row[1],row[2],row[3],row[4]])
+
+            if len(data)==0:
+                con.close()
+                return render_template("login.html",msg="IDが間違っています")
+            
+            if cph(data[0][4],passwd):
+                # ログイン情報をセッションに記録する
+                session["name"]=data[0][0]
+                session["id"]=data[0][3]
+                con.close()
+                return redirect("home")
+                
+            else:
+                con.close()
+                return render_template("login.html",msg="パスワードが間違っています")
+
 
 #マイページ
-@app.route('/mypage',methods=["POST","GET"])
+@app.route("/home")
 def home():
-        if request.method == "GET": 
-                name="nojiri"
-                money=123456
-                return render_template("mypage.html",name=name, money=money)
-        elif request.method == "POST":
-                return redirect("record")
+    if "name" in session:
+        # セッションに記録した情報を出力
+        id=session["id"]
+        con = connect()
+        cur = con.cursor()
+        cur.execute("""
+        SELECT money
+        FROM balance
+        WHERE id=%(id)s""",{"id":id})
+        data=[]
+        for row in cur:
+                data.append(row)
+        money=data[0][0]
+        con.close()
+        return render_template("mypage.html",
+        id=id,
+        money=money,
+        name=html.escape(session["name"])) 
+
+    else:
+        # セッションがない場合ログイン画面にリダイレクト
+        return redirect("login")
+
+
+
+
+
+# 収入支出
+#@app.route('/mypage',methods=["POST","GET"])
+#def home():
+#        if request.method == "GET": 
+#                name="nojiri"
+#                money=123456
+#                return render_template("mypage.html",name=name, money=money)
+#        elif request.method == "POST":
+#                return redirect("record")
+
 
 #入出金の記録
 @app.route('/record',methods=["POST","GET"])
